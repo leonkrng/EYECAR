2a7f28b5fadafb68c96f6d55841fff68634741a4
Daniel Grunau <daniel.grunau@homag.de>
2025-12-07 17:16:43 +0100

    deleted ultrasonic stuff on Arduino nano

--- Mainboard_Nano/src/main.cpp
+++ Mainboard_Nano/src/main.cpp
@@ -6,19 +6,6 @@
 //Pramoth.T
 
 #include<SPI.h>
-#include <NewPing.h>
-
-#define SONAR_NUM 4      // Number of sensors.
-#define MAX_DISTANCE 250 // Maximum distance (in cm) to ping.
-
-#define SENSORENGLEICHZEITIG false
-
-NewPing sonar[SONAR_NUM] = {   // Sensor object array.
-  NewPing(2, 3, MAX_DISTANCE),
-  NewPing(4, 5, MAX_DISTANCE),
-  NewPing(6, 7, MAX_DISTANCE),
-  NewPing(8, 9, MAX_DISTANCE) //trigPin, echoPin, max distance in cm
-};
 
 volatile boolean received;
 volatile byte SPIRECEIVED,SPISEND;
@@ -28,10 +15,8 @@ volatile byte dataVomESP[20];
 
 int buttonvalue;
 int x;
-byte pingNummer;
 
-int distanz[4];
-unsigned long currentMillis, lastUltrasonicPing, lastPrint;
+unsigned long currentMillis, lastPrint;
 
 bool blink;
 bool beleuchtungAktiv;
@@ -90,60 +75,24 @@ void loop(){
     //Serial.print("I-> "); Serial.println(SPIRECEIVED);    
     received = false; 
   }
-  
-
-
-
-  #if SENSORENGLEICHZEITIG
-
-    if(currentMillis - lastUltrasonicPing > 100) {
-      for (uint8_t i = 0; i < SONAR_NUM; i++) { // Loop through each sensor and display results.
-          distanz[i] = sonar[i].ping_cm();
-        }
-
-      lastUltrasonicPing = currentMillis; 
-      //Serial.print("Distanz: "); Serial.print(distanz[0]); Serial.print("   "); Serial.print(distanz[1]);  Serial.print("   "); Serial.print(distanz[2]);  Serial.print("   "); Serial.println(distanz[3]); 
-
-
-    }
-
-  #else
-    if(currentMillis - lastUltrasonicPing > 50) {   
-      distanz[pingNummer] = sonar[pingNummer].ping_cm();
-      pingNummer >= 3 ? pingNummer = 0 : pingNummer++;    //Pingnummer erhöhen bzw auf 0 setzen
-      
-      lastUltrasonicPing = currentMillis; 
-      //Serial.print("Distanz: "); Serial.print(distanz[0]); Serial.print("   "); Serial.print(distanz[1]);  Serial.print("   "); Serial.print(distanz[2]);  Serial.print("   "); Serial.println(distanz[3]); 
-
-
-    }
-
-  #endif
-
-
-  dataZumESP[0] = distanz[0];
-  dataZumESP[1] = distanz[1];
-  dataZumESP[2] = distanz[2];
-  dataZumESP[3] = distanz[3];
 
   if(currentMillis - lastPrint > 100) {
     lastPrint = currentMillis;
 
-    Serial.print(dataVomESP[0]);
+   /* Serial.print(dataVomESP[0]);
     Serial.print(dataVomESP[1]);
     Serial.print(dataVomESP[2]);
     Serial.print(dataVomESP[3]);
     Serial.print(dataVomESP[4]);
     Serial.println(dataVomESP[5]);
-
+    */
   }
 
   if(dataVomESP[0] == 1) {
     beleuchtungAktiv = dataVomESP[1];
   }
 
-  io();
-  
+  io();  
 }
 
 
