e7a789a0159351f5c339fb9d6bd95779e9f0f424
leonkrng <kroening.social@gmail.com>
2025-12-12 14:54:21 +0100

    Fixed freezing camera stream (probably)

--- vision_raspberry_pi/overlay/draw_overlay.py
+++ vision_raspberry_pi/overlay/draw_overlay.py
@@ -191,4 +191,6 @@ def draw_overlay(frame,
     cv2.moveWindow(window_name, 0, 36)
     cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     cv2.resizeWindow(window_name, 832, 600)
+    cv2.waitKey(1)
     cv2.imshow(window_name, frame)
+
