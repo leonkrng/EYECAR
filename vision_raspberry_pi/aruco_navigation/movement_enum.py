d41e87e862d56e9990d3385a13f71567a2e696b8
leonkrng <kroening.social@gmail.com>
2025-12-18 12:36:33 +0100

    Update Movement_Enum to values from Mainboard_Esp32/include/movementDirections.h

--- vision_raspberry_pi/aruco_navigation/movement_enum.py
+++ vision_raspberry_pi/aruco_navigation/movement_enum.py
@@ -3,10 +3,19 @@ from pickle import STOP
 
 class MovementEnum(Enum):
     STOP = 0
-    NO_MARKER = 1
-    ALIGN_RIGHT = 2
-    FORWARD = 3
-    ALIGN_LEFT = 4
-    SEARCH_NEXT = 5
-    TOO_CLOSE = 6
+    FORWARD = 1
+    BACKWARD = 2
+    LEFT = 3
+    RIGHT = 4
+    FORWARD_LEFT = 5
+    FORWARD_RIGHT = 6
+    BACKWARD_LEFT = 7
+    BACKWARD_RIGHT = 8
+    TURN_LEFT = 9
+    TURN_RIGHT = 10
+    FORWARD_TURN_LEFT = 11
+    FORWARD_TURN_RIGHT = 12
+    BACKWARD_TURN_LEFT = 13
+    BACKWARD_TURN_RIGHT = 14
+
 
