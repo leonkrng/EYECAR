2e049d0704fbf06b77bfc9a9eb3f31ca4892204a
leonkrng <kroening.social@gmail.com>
2026-01-11 18:10:05 +0100

    Fixed draw_marker_boarder

--- vision_raspberry_pi/overlay/draw_marker_border.py
+++ vision_raspberry_pi/overlay/draw_marker_border.py
@@ -1,16 +1,26 @@
 import cv2
 
 # Draws a border box around the marker
-def draw_marker_border(frame, corners, id):
+def draw_marker_border(frame, corners, marker_id):
 
-    (top_left, top_right, bottom_right, bottom_left) = corners
-    cv2.line(frame, top_left, top_right, (0, 255, 0), 2)
-    cv2.line(frame, top_right, bottom_right, (0, 255, 0), 2)
-    cv2.line(frame, bottom_right, bottom_left, (0, 255, 0), 2)
-    cv2.line(frame, bottom_left, top_left, (0, 255, 0), 2)
+    (tl, tr, br, bl) = corners
 
-    cv2.putText(frame, str(id),
-        (top_left[0], top_left[1] - 15),
+    tl = tuple(map(int, tl))
+    tr = tuple(map(int, tr))
+    br = tuple(map(int, br))
+    bl = tuple(map(int, bl))
+
+    cv2.line(frame, tl, tr, (0, 255, 0), 2)
+    cv2.line(frame, tr, br, (0, 255, 0), 2)
+    cv2.line(frame, br, bl, (0, 255, 0), 2)
+    cv2.line(frame, bl, tl, (0, 255, 0), 2)
+
+    cv2.putText(
+        frame,
+        str(int(marker_id)),
+        (tl[0], tl[1] - 15),
         cv2.FONT_HERSHEY_SIMPLEX,
-        1, (0, 255, 0), 3)
-
+        1,
+        (0, 255, 0),
+        3
+    )
