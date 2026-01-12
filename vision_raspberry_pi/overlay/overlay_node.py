14f7cdd9eac198a4384d44113e1841098bcc05a0
leonkrng <kroening.social@gmail.com>
2026-01-12 14:59:29 +0100

    Changed Window properties

--- vision_raspberry_pi/overlay/overlay_node.py
+++ vision_raspberry_pi/overlay/overlay_node.py
@@ -18,18 +18,18 @@ class OverlayNode(Node):
         self.bridge = CvBridge()
 
         self.last_frame = None
-        cv2.namedWindow("EYE-CAR", cv2.WND_PROP_FULLSCREEN)
-        cv2.moveWindow("EYE-CAR", 0, 36)
-        cv2.resizeWindow("EYE-CAR", 832, 600)
-        self.frame_timer = self.create_timer(0.03, self.timer_callback)
+        cv2.namedWindow("EYECAR", cv2.WINDOW_NORMAL)
+        cv2.setWindowProperty("EYECAR", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+        self.frame_timer = self.create_timer(0.03, self.show_window_callback)
 
     def processed_frame_callback(self, msg:Image):
 
         frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
         self.last_frame = draw_overlay(frame)
 
-    def timer_callback(self):
+    def show_window_callback(self):
         if self.last_frame is not None:
                 frame = draw_overlay(self.last_frame)
-                cv2.imshow("EYE-CAR", frame)
+                cv2.imshow("EYECAR", frame)
                 cv2.waitKey(1)
